d95935915df7394e2d967bb9155ee81fa1cc874f
Thiago Anselme <anselme.thiago@gmail.com>
2020-07-30 08:43:26 -0300

    The exercice 1 has been done

---
+++ src/Anselme.Study.EntityFramework/EF-Console/DataAccess/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using Anselme.Study.EntityFramework.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anselme.Study.EntityFramework.DataAccess
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext") { }
+
+        public DbSet<Students> Students { get; set; }
+
+    }
+}
