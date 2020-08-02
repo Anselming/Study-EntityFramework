adf3925516525fb68440b6b9ee97b9786e3f2590
Thiago Anselme <anselme.thiago@gmail.com>
2020-08-02 09:13:55 -0300

    Now, that's correct Ex 2 commit

---
+++ src/Anselme.Study.EntityFramework/Anselme.Study.EntityFramework/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anselme.Study.EntityFramework
+{
+    public class DataAccess
+    {
+        public DbSet<Student> Student()
+        {
+            return 
+        }
+    }
+}
